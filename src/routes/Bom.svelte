e6e9b256fa2c7448257ce26c12786dfbe63a72f5
Tanner Plauche <tannerplauche@gmail.com>
2024-07-23 15:48:44 -0500

    Add space between chapters on mobile

--- src/routes/Bom.svelte
+++ src/routes/Bom.svelte
@@ -12,7 +12,7 @@
 <div>
     <h1>Books:</h1>
     <div class="langSelection">
-       Select Language  <select id="selectLang" bind:value={languageCode}>
+        Select Language <select id="selectLang" bind:value={languageCode}>
             {#each languageCodes as language}
                 <option value={language.code}>{language.languageName}</option>
             {/each}
@@ -24,13 +24,13 @@
                 <h3>{book.book}</h3>
                 {#each book.chapters as chapter, index}
                     <a
+                        class="chapter"
                         href="https://www-churchofjesuschrist-org.translate.goog/study/scriptures/bofm/{book.abbreviation}/{index +
                             1}?lang=eng&_x_tr_sl=en&_x_tr_tl={languageCode}&_x_tr_hl=en&_x_tr_pto=wapp&_x_tr_hist=true"
                         target="_blank"
                     >
                         {chapter.reference}
                     </a>
-                    <br />
                 {/each}
             </div>
         {/each}
@@ -45,7 +45,16 @@
     }
 
     .book {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
         padding: 1rem;
+    }
+    @media screen and (max-width: 480px) {
+        .chapter {
+            margin-bottom: 0.5rem;
+        }
     }
 
     .langSelection {
